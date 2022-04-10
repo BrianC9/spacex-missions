ae8b2aad562e24e061d4210545a511d6ac6c89a2
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-04-10 19:36:32 +0200

    react dia 2: react router

--- src/components/LaunchesList.jsx
+++ src/components/LaunchesList.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Heading, Box } from "@chakra-ui/react";
+import { Heading, Box, Text } from "@chakra-ui/react";
 import Launch from "./Launch";
 import * as API from "../services/launches";
 
